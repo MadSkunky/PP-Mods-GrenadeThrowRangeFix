73338855664ef865df62ad6645be39287249e37e
MadSkunky <94592595+MadSkunky@users.noreply.github.com>
2022-01-04 10:17:36 +0100

    Projektdateien hinzufügen.

---
+++ GrenadeThrowRangeFix.cs
@@ -0,0 +1,48 @@
+﻿using Harmony;
+using PhoenixPoint.Tactical.Entities.Weapons;
+using System;
+
+namespace MadSkunky.GrenadeThrowRangeFix
+{
+    // New config class.
+    internal class ModConfig
+    {
+        public int ThrowRangeDivisor = 12;
+    }
+    public class GrenadeThrowRangeFix
+    {
+        // New config field.
+        internal static ModConfig Config;
+        public static void MainMod(Func<string, object, object> api)
+        {
+            // Read config and assign to field.
+            Config = api("config", null) as ModConfig ?? new ModConfig();
+            HarmonyInstance.Create("GrenadeThrowRangeFix.MadSkunky").PatchAll();
+            api("log verbose", "Mod Initialised.");
+        }
+        // This "tag" allows Harmony to find this class and apply it as a patch.
+        [HarmonyPatch(typeof(Weapon), "GetThrowingRange")]
+        // Class can be any name, but must be static.
+        internal static class GetThrowingRange_fix
+        {
+
+            // Overwrite original fuction to calculate throwing range for grenades.
+            [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051")]
+            private static bool Prefix(ref float __result, Weapon __instance, float rangeMultiplier)
+            {
+                try
+                {
+                    float num = __instance.TacticalActor.CharacterStats.Endurance * __instance.TacticalActor.TacticalActorDef.EnduranceToThrowMultiplier;
+                    float num2 = __instance.TacticalActor.CharacterStats.BonusAttackRange.CalcModValueBasedOn(num);
+                    num = num * __instance.GetDamagePayload().Range / Config.ThrowRangeDivisor;
+                    __result = num / (float)__instance.Weight * rangeMultiplier + num2;
+                    return false;
+                }
+                catch (Exception)
+                {
+                    return true;
+                }
+            }
+        }
+    }
+}
---
+++ mod_info.js
@@ -0,0 +1,9 @@
+﻿{
+    Id: "MadSkunky.GrenadeThrowRangeFix", // Must be unique. Same Id = one survives
+    Lang : "en",
+    Name : "Grenade throwrange fix",
+    Url  : "",
+    Description : "Fixes the throw range calculation for grenades, taking the defined range of each grenade into account",
+    Requires : { Id: "Modnix", Min: "3.0" },
+    ConfigType: "MadSkunky.GrenadeThrowRangeFix.ModConfig", // Fullname with namespace
+}
